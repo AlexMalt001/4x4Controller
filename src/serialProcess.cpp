064ffe423355d8f2f4903d6375d29c4624572bc8
AlexMalt001 <alexmalt001@btinternet.com>
2017-03-27 10:35:32 +0100

    BEGAN WORK ON REDESIGN OF mapping code

--- src/serialProcess.cpp
+++ src/serialProcess.cpp
@@ -7,22 +7,12 @@ void serialProcess :: sendVars(int th, int st) {
 //  Serial1.println(st + "," + th);
 }
 
-int dataProcess :: processVars(int input, int upper, int lower, int deadzone, int upperPWM, int lowerPWM) {
-  //TODO: REDO
-  int range = upper - lower;
-  int half = (((upper - lower)/2) + lower);
-  int upperDZ = half + deadzone;
-  int lowerDZ = half - deadzone;
-
-  if(upperDZ > input && input > lowerDZ) {
-    input = half;
+int dataProcess :: processVars(int input, int upper, int lower, int deadzone, int upperPWM, int lowerPWM, int theoUpper, int theoLower) {
+  //TODO: CHANGE HEADER FOR MAX/MIN VALUES
+  int half = theoLower + (theoUpper - theoLower);
+  if(input > (half-deadzone) && input < (half+deadzone)) {
+    return //TODO ADD HALFPWM;
   }
-
-  int presult = input - lower;
-  int presult2 = (presult*100) / (range);
-  Serial.println(presult2);
-  int result = (presult*(upperPWM-lowerPWM))+lowerPWM;
-  return result;
 }
 
 void screenDataProcess::prepareScreenData(int *ptr) {
